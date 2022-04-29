1090ad835ad41e45d4bac70ea6ef76f214a2bd4d
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-29 15:58:43 +0100

    Fix bug with turbo redirection

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,6 +8,34 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+
+# class TurboFailureApp < Devise::FailureApp
+#   def respond
+#     if request_format == :turbo_stream
+#       redirect
+#     else
+#       super
+#     end
+#   end
+
+#   def skip_format?
+#     %w(html turbo_stream */*).include? request_format.to_s
+#   end
+# end
+
+# Devise.setup do |config|
+
+#   # ==> Controller configuration
+#   # Configure the parent class to the devise controllers.
+#   config.parent_controller = 'TurboController'
+#   config.navigational_formats = ['*/*', :html, :turbo_stream]
+
+#   # ==> Warden configuration
+#   config.warden do |manager|
+#     manager.failure_app = TurboFailureApp
+#   end
+# end
+
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -264,6 +292,7 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -308,4 +337,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  
 end
